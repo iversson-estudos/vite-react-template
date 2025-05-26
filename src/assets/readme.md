8a4287b2420a0a16f9d095a44b657ee636a6ddc3
iversson-estudos <i versson.estudos@gmail.com>
2025-05-26 09:02:40 -0300

    global and reset css added and executed on main.jsx

---
+++ src/assets/readme.md
@@ -0,0 +1,1 @@
+This folder will be used for: Imgs SVG's Global CSS
--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+/*CSS IMPORTS*/
+import './assets/css/reset.css';   // Reset first
+import './assets/css/global.css'; // Then globals
+
+/*OTHER IMPORTS*/
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
